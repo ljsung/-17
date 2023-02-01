e2ac8242544bdc1c199b86090c130c036f6eb0ba
ljsung <ljsung6@naver.com>
2023-02-01 18:48:32 +0900

    챕터 17

---
+++ src/com/greedy/section02/layout/run/Application.java
@@ -0,0 +1,19 @@
+package com.greedy.section02.layout.run;
+
+import com.greedy.section02.layout.A_BorderLayout;
+import com.greedy.section02.layout.B_FlowLayout;
+import com.greedy.section02.layout.C_GridLayout;
+import com.greedy.section02.layout.D_CardLayout;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+//		new A_BorderLayout();
+//		new B_FlowLayout();
+//		new C_GridLayout();
+		new D_CardLayout();
+		}
+	}
+
+
