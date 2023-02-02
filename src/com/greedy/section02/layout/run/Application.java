a8c879a46fcf988e2bf94523310bcd93ff00ac1f
ljsung <ljsung6@naver.com>
2023-02-02 18:21:43 +0900

    챕터17 이번에 팀 프로젝트 할 때 참고 해야함.

--- src/com/greedy/section02/layout/run/Application.java
+++ src/com/greedy/section02/layout/run/Application.java
@@ -1,9 +1,7 @@
 package com.greedy.section02.layout.run;
 
-import com.greedy.section02.layout.A_BorderLayout;
-import com.greedy.section02.layout.B_FlowLayout;
-import com.greedy.section02.layout.C_GridLayout;
-import com.greedy.section02.layout.D_CardLayout;
+import com.greedy.section02.layout.E_NullLayout;
+import com.greedy.section02.layout.F_PanelLayout;
 
 public class Application {
 
@@ -12,7 +10,9 @@ public class Application {
 //		new A_BorderLayout();
 //		new B_FlowLayout();
 //		new C_GridLayout();
-		new D_CardLayout();
+//		new D_CardLayout();
+		new E_NullLayout();
+		new F_PanelLayout();
 		}
 	}
 
